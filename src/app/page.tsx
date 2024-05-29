5cbe5f75c2f7aa8099660560d32f1da862f0dab4
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 22:28:46 +0000

    Auth pages and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-
-import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
 
 export default function Home() {
-  return <Dashboard user_id="66569f982b399320e74304ac" />
+  // return <Dashboard user_id="66569f982b399320e74304ac" />
+  
+  return (<main  suppressHydrationWarning={true}>
+    <Login />
+  </main>)
 }
