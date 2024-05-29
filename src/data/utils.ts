bd8bcf5011c7217da90ce501d72ce06e03b55e87
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 04:03:05 +0000

    Helper functions

---
+++ src/data/utils.ts
@@ -0,0 +1,56 @@
+type ResponseStructure = {
+  data: any;
+  error: boolean;
+  stack: any;
+}
+
+export function craftQueryParams(queryParams: {[key: string]: string}) {
+  let returnString = '';
+  for (let key in queryParams) {
+    returnString += `${key}=${queryParams[key]}&`
+  }
+  const cleanedString = returnString.substring(0, returnString.length - 1);
+  return cleanedString;
+}
+
+export async function sendFetchRequest(endpoint: string, method: string, body: any) {
+  let bodyVal = {};
+  let queryParams = '';
+
+  if(['GET', 'DELETE'].includes(method)) {
+    queryParams = `?${craftQueryParams(body)}`;
+  } else {
+    bodyVal = {
+      body: JSON.stringify(body),
+    }
+  }
+  console.log(method, bodyVal, 'METHOD AND BODY VALLL')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}${endpoint}${queryParams}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...bodyVal,
+  });
+  const res_val = await res.json();
+  console.log(res_val, res, 'RESPONSESSSSRAWWWW')
+  return res_val;
+}
+
+export function formatDate2(dateString: string): string {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
+
+export const yearsArray = (currentYear: number): number[] => {
+  const returnArr = [];
+  for (let i = currentYear - 5; i <= currentYear; i++) {
+    returnArr.push(i);
+  }
+  return returnArr;
+}
