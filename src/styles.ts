0be48c67b0bd56f3f63ec68e7f45166b4d0f3204
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 03:59:13 +0000

    Add styling file

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+export const styles = {
+  title: "text-[20px] font-[500] text-center py-2",
+  label: "text-[14.4px] text-black",
+  input:
+    "w-full text-[14px] border rounded h-[34px] text-black px-2 outline-none mt-[4px]",
+  form_input:
+    "w-full text-[14px] bg-slate-700 border rounded text-black px-2 py-2 outline-none mt-[4px]",
+  button:
+    "flex flex-row justify-center item-center py-2 rounded-full cursor-pointer bg-[#727cf5] min-h-[40px] w-full text-[13px] font-[600] text-black",
+  small_btn:
+    "flex flex-row justify-center item-center py-2 rounded-full cursor-pointer bg-[#111827] hover:bg-[#1f2937] items-center min-h-[30px] w-[150px] text-[13px] font-[600] text-black transition-all duration-300",
+  wide_tb_th: "min-w-[200px] relative py-2",
+  sort_span: "flex justify-center gap-2 items-center relative cursor-pointer",
+};
