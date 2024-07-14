b41b980857a4173dcd7413767c26438536109eb2
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-14 09:02:37 +0000

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import Login from "@/components/Login";
 
 export default function Home() {
-  // return <Dashboard user_id="66569f982b399320e74304ac" />
-  
   return (<main  suppressHydrationWarning={true}>
     <Login />
   </main>)
