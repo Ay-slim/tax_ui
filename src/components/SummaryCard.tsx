c6abb54e081c172995b70bec2711c0edb11c2d63
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-18 19:13:31 +0000

    Update summary card to not spill on shrinking

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -1,7 +1,7 @@
 const SummaryCard = ({title, value}: {title: string; value: string;}) => {
   return (
-    <div className="w-1/2 flex-col justify-center text-center py-10 items-center h-full bg-gray-800">
-      <p className="text-sm">{`${title}:`}</p> <strong><p className="text-xl">{`${value}`}</p></strong>
+    <div className="w-1/2 grid grid-rows-2 justify-center text-center py-10 items-center h-full bg-gray-800">
+      <p className="text-sm break-words self-start">{`${title}:`}</p> <strong><p className="text-xl break-words self-start">{`${value}`}</p></strong>
     </div>
   )
 }
