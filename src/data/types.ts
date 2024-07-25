4fa0cd2a450ad5961c5ef9dd6012f32e086d3054
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-25 23:27:08 +0000

    Dashboard data type update

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,3 +1,35 @@
+type NameAndAmount = {
+  name: string;
+  amount: number;
+  reason: string;
+};
+
+export type DashboardData = {
+  summary: {
+      user_id: string,
+      total_taxed_income: number,
+      total_income: number,
+      total_deducted_tax: number,
+      current_tax_index: number,
+      current_tax_bracket: string,
+      country: string,
+      currency: string,
+      country_id: string,
+      taxes: NameAndAmount[],
+      deductions: NameAndAmount[],
+  },
+  filings: {
+          _id: string,
+          user_id: string,
+          description: string,
+          amount: number,
+          category: string,
+          date: string,
+          created_at: Date,
+      }[],
+  years: number[]
+}
+
 export type ContributionsUnit = {
   name: string;
   percentage: number;
