df011a7ddc0c3540fcce50cf1afe1b95ccd1ae11
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-26 10:55:59 +0000

    Implement updates to income filing flow

--- src/components/FileIncome.tsx
+++ src/components/FileIncome.tsx
@@ -4,9 +4,11 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { fileIncome } from "@/data/api_calls";
-import { Card, Input, Space } from "antd";
+import { fetchUserContributions, fileIncome } from "@/data/api_calls";
+import { Card, Input, Space, Select, SelectProps } from "antd";
 import { FaTimes } from "react-icons/fa";
+import { styles } from "@/styles";
+import { filingCategoryMap } from "@/data/utils";
 
 const schema = Yup.object().shape({
   amount: Yup.string().required("Amount to file is required"),
@@ -16,28 +18,46 @@ const schema = Yup.object().shape({
 const FileIncome = (props: {user_id: string, country_id: string, setClickedNew: any}) => {
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
+  const [contributionOptions, setContributionOptions] = useState<SelectProps[]>([]);
+
+  useEffect(() => {
+    const contribOptionsFetch = async () => {
+      const contribResponse = await fetchUserContributions({
+        user_id: props.user_id,
+        country_id: props.country_id, 
+      });
+      setContributionOptions(contribResponse as SelectProps[]);
+    }
+    contribOptionsFetch();
+  }, []);
 
   const formik = useFormik({
     initialValues: {
       amount: "",
       description: "",
       date: "",
+      category: "",
+      contributions: [],
     },
     validationSchema: schema,
     onSubmit: async ({
       amount,
       description,
-      date
+      date,
+      category,
+      contributions
     }) => {
       try{
         // console.log(values, 'VALUESSS');
         // console.log(amount, description, year, 'AMONEIFEEDE')
         await fileIncome({
           user_id: props.user_id,
-          income: Number(amount),
+          amount: Number(amount),
           description,
           date,
           country_id: props.country_id,
+          category,
+          contributions,
         });
         // console.log(filedIncome, 'FILED INCOMEEEE');
         setIsSuccess(true);
@@ -63,9 +83,6 @@ const FileIncome = (props: {user_id: string, country_id: string, setClickedNew: 
     }
   }, [isSuccess, error]);
 
-  const { values, handleSubmit } =
-    formik;
-
   return (
     <>
       <div className="max-sm:min-h-[100vh] min-h-[70vh] flex flex-col justify-center sm:px-6 lg:px-8 z-[-100px] mt-10">
@@ -89,32 +106,58 @@ const FileIncome = (props: {user_id: string, country_id: string, setClickedNew: 
             <div className="mt-4 flex flex-col items-center justify-center">
               <Space direction="vertical">
                 <div className="w-full">
-                  <span className="text-[12px]">Description:</span>
-                  <Input
-                    style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}
-                    className="h-[45px] text-[12px] shadow-md"
-                    value={values.description}
-                    placeholder="Income description"
-                    onChange={(e) => formik.setFieldValue("description", e.target.value)}
-                  />
-                </div>
-                <div className="w-full">
                   <span className="text-[12px]">Amount:</span>
                   <Input
                     style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}
                     className="h-[45px] text-[12px] shadow-md"
-                    value={values.amount?.toLocaleString()}
+                    value={formik.values.amount?.toLocaleString()}
                     placeholder="Income amount"
                     onChange={(e) => formik.setFieldValue("amount", Number(e.target.value.replace(/,/g, "")))}
                   />
                 </div>
+                <div className="w-full">
+                  <span className="text-[12px]">Description:</span>
+                  <Input
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}
+                    className="h-[45px] text-[12px] shadow-md"
+                    value={formik.values.description}
+                    placeholder="Income description"
+                    onChange={(e) => formik.setFieldValue("description", e.target.value)}
+                  />
+                </div>
+                <label htmlFor="email" className="mt-2 text-[12px]">
+                  Category:
+                </label>
+                <Select
+                  defaultValue="--------"
+                  className="h-[40px] text-[12px] shadow-md w-full"
+                  style={{ width: '100%' }}
+                  onChange={(value) => formik.setFieldValue("category", value)}
+                  options={Object.keys(filingCategoryMap)?.map(option => ({
+                    label: filingCategoryMap[option],
+                    value: option,
+                  }))}
+                />
+                <label className="mt-2 text-[12px]">
+                  Contributions:
+                </label>
+                <Select
+                  mode="multiple"
+                  className="h-[45px] text-[12px] shadow-md"
+                  allowClear={true}
+                  style={{ width: '100%' }}
+                  placeholder="Select contributions"
+                  defaultValue={[]}
+                  onChange={(value: string[]) => formik.setFieldValue("contributions", [...value])}
+                  options={contributionOptions.map(contr => ({label: contr, value: contr}))}
+                />
                 <div className="w-full">
                   <span className="text-[12px]">Date:</span>
                   <Input
                     type="date"
                     style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}
                     className="h-[45px] text-[12px] shadow-md"
-                    value={values.date}
+                    value={formik.values.date}
                     placeholder="Income amount"
                     onChange={(e) => formik.setFieldValue("date", e.target.value)}
                   />
