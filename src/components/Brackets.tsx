10f6f3a8ce788cb520cdae3f8a28416c7a54d334
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-30 20:18:51 +0000

    Bracket component updates (deprecated)

--- src/components/Brackets.tsx
+++ src/components/Brackets.tsx
@@ -3,8 +3,8 @@ import { FaTimes } from "react-icons/fa";
 
 const Brackets = ({name, brackets, setBrackets}: {name: string; brackets: string[]; setBrackets: Dispatch<SetStateAction<string[]>>}) => {
   return (
-    <>
-      <div className="max-sm:min-h-[100vh]flex flex-col justify-center sm:px-6 lg:px-8 -top-0 mt-24 right-10 absolute bg-white text-black rounded-md py-10 opacity-80">
+    <div>
+      <div>
         <FaTimes
             className="close-icon text-red-600 cursor-pointer absolute top-0 right-5 mt-4 mr-4 hover:bg-red-300"
             onClick={() => setBrackets([])}
@@ -16,7 +16,7 @@ const Brackets = ({name, brackets, setBrackets}: {name: string; brackets: string
           {brackets.map((bracket, idx) => {return <span key={idx}>{bracket}</span>})}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
