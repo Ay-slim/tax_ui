ecb80adadc22f94469bd03f5d6f400633b8c9e78
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-30 20:17:08 +0000

    Inject more info modal into summary card component

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -1,8 +1,32 @@
-const SummaryCard = ({title, value}: {title: string; value: string;}) => {
+import { useState } from "react";
+import MoreInfoModal from "./MoreInfoModal";
+import { MoreInfoTableColumns, MoreInfoTableDataSource } from "@/data/types";
+
+const SummaryCard = ({
+  title,
+  value,
+  infoText,
+  info,
+}: {
+    title: string;
+    value: string;
+    infoText?: string;
+    info?: {
+      title: string;
+      dataSource: MoreInfoTableDataSource[];
+      columns: MoreInfoTableColumns[];
+    };
+  }) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
-    <div className="w-1/2 grid grid-rows-2 justify-center text-center py-10 items-center h-full bg-gray-800">
-      <p className="text-sm break-words self-start">{`${title}:`}</p> <strong><p className="text-xl break-words self-start">{`${value}`}</p></strong>
-    </div>
+    <>
+      <MoreInfoModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} info={info || null} />
+      <div className="relative w-1/2 grid grid-rows-2 justify-center text-center py-10 items-center h-full bg-gray-800">
+        {infoText && <p className="absolute top-2 left-2 text-[8px] underline italic hover:text-blue-800 cursor-pointer" onClick={() => setIsModalOpen(!isModalOpen)}>{infoText}</p>}
+        <p className="text-sm break-words self-start">{`${title}:`}</p>
+        <strong><p className="text-xl break-words self-start">{`${value}`}</p></strong>
+      </div>
+    </>
   )
 }
 
